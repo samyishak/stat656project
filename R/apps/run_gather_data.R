357991a251104d82b9d6231a37580333a9c44d3b
Samy Ishak <s.ishak.b@gmail.com>
2021-10-31 19:19:49 -0700

    merge all datasets, write out merged data

--- R/apps/run_gather_data.R
+++ R/apps/run_gather_data.R
@@ -1,28 +1,37 @@
-# requests all data
-# checks for data you already have and doesn't request that unless you have that set to truncate
-#
+# Gathers data that isn't already available locally
+# Merges all data on date
 
 source("R/components/request_data.R")
 
-main <- function() {
+output_path <- "data/raw"
+dir.create(output_path, showWarnings=FALSE, recursive=TRUE)
 
-  output_path <- "data/raw"
-  dir.create(output_path, showWarnings=FALSE, recursive=TRUE)
+# check local for existing datasets
+existing_df_names <- stringr::str_remove(list.files(output_path), "\\.rds$")
 
-  # check local for existing datasets
-  existing_dfs <- stringr::str_remove(list.files(output_path), "\\.rds$")
+# get the setdiff between existing and full list
+new_requests <- setdiff(names(FULL_REQUEST_LIST), existing_df_names)
 
-  # get the setdiff between existing and full list
-  new_requests <- setdiff(names(FULL_REQUEST_LIST), existing_dfs)
+# request data on the setdiff
+df_list <- request_data(FULL_REQUEST_LIST[new_requests])
 
-  # request data on the setdiff
-  df_list <- request_data(FULL_REQUEST_LIST[new_requests])
-
-  # write new datasets to local
-  for (df_name in names(df_list)) {
-    readr::write_rds(df_list[[df_name]], file.path(output_path, glue::glue("{df_name}.rds")))
-  }
+# write new datasets to local
+for (df_name in names(df_list)) {
+  readr::write_rds(df_list[[df_name]], file.path(output_path, glue::glue("{df_name}.rds")))
 }
 
-# Run the app
-main()
+rm(df_list)
+
+# read all dataframes
+all_df_paths <- list.files(output_path, full.names=TRUE)
+df_list <- list()
+for (df_path in all_df_paths) {
+  elem <- readr::read_rds(df_path) %>% list()
+  df_list <- append(df_list, elem)
+}
+
+# merge datasets
+df <- df_list %>% purrr::reduce(full_join, by="date")
+
+# write merged dataset
+readr::write_rds(df, file.path(output_path, "_all_data.rds"))
